6079329b120de49e18ef4fdb3b3589ce5b9ea295
Trada Savan <81802024+savantrada0@users.noreply.github.com>
2023-02-18 17:18:05 +0530

    sf

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
 
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
